44e43629698cdb3bfe50f9cb27d291cb021cffd2
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-09-10 23:04:31 -0700

    trying to create store using create-react-app-redux

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />,
+    </Provider>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
