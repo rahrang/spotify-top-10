cc8823a444e30c08aafcdf2aad3f173500f2d0ee
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-09-10 21:51:56 -0700

    found way to import files to frontend, need to push data to redux store in componentdidmount

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,11 +9,20 @@ import { css, StyleSheet } from 'aphrodite';
 
 const helpers = require('../client/helpers.js');
 
+const daily_dates = require('../client/daily_dates.json');
+
 class Home extends React.Component {
 
   render() {
 
-    console.log(helpers.getDates('daily_dates.json'));
+    console.log(daily_dates);
+
+
+    // find ways to push this to Redux store in ComponentDidMount
+    for (var i = 0; i < daily_dates.length; i++) {
+      var dateFile = require(`../client/daily_data/${daily_dates[i]}.json`);
+      console.log(dateFile);
+    }
 
     return (
       <div className="home-container">
